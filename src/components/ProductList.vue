42ab8f7131ee79a3afadeb0f2031c1d0e1955034
marlenesedano <marlenesedanodasilva@gmail.com>
2022-01-11 19:15:51 -0400

    feat: home page styles

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,32 +1,51 @@
 <template>
   <section class="products-container">
-    <div v-for="product in products" :key="product.id">
-      <img
-        v-if="product.fotos"
-        :src="product.fotos[0].src"
-        alt="product.fotos[0].titulo"
-      />
-      <p class="price">{{ product.preco }}</p>
-      <h2 class="title">{{ product.nome }}</h2>
-      <p>{{ product.descricao }}</p>
+    <div class="products" v-if="products && products.length">
+      <div class="product" v-for="product in products" :key="product.id">
+        <router-link to="/">
+          <img
+            v-if="product.fotos"
+            :src="product.fotos[0].src"
+            alt="product.fotos[0].titulo"
+          />
+          <p class="price">{{ product.preco }}</p>
+          <h2 class="title">{{ product.nome }}</h2>
+          <p>{{ product.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="products && products.length === 0">
+      <p class="no-result">Sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
 
 <script>
 import { api } from "../services.js";
+import { serialize } from "../helpers";
 export default {
   data() {
     return {
       products: null,
+      productPage: 9,
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.productPage}${query}`;
+    },
   },
   methods: {
     getProducts() {
-      api.get("produto").then((response) => {
-        console.log("teste", response);
+      api.get(this.url).then((response) => {
         this.products = response.data;
       });
+    },
+  },
+  watch: {
+    url() {
+      this.getProducts();
     },
   },
   created() {
@@ -35,4 +54,49 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.products-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.products {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.product {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.product:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.product-img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.title {
+  margin-bottom: 10px;
+}
+
+.price {
+  color: #e80;
+  font-weight: bold;
+}
+
+.no-result {
+  text-align: center;
+}
+</style>
