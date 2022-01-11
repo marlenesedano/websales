f42b6cf847c482cc84d555aaa38485816ba9138f
marlenesedano <marlenesedanodasilva@gmail.com>
2022-01-10 21:14:18 -0400

    feat: added product components

---
+++ src/components/ProductList.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="products-container">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.fotos"
+        :src="product.fotos[0].src"
+        alt="product.fotos[0].titulo"
+      />
+      <p class="price">{{ product.preco }}</p>
+      <h2 class="title">{{ product.nome }}</h2>
+      <p>{{ product.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  methods: {
+    getProducts() {
+      fetch("http://localhost:3000/produto")
+        .then((response) => response.json())
+        .then((response) => {
+          this.products = response;
+        });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,29 @@
 <template>
   <section>
-    <h1></h1>
+    <h1>Compra e Venda</h1>
+    <ProductSearch />
+    <ProductList />
   </section>
 </template>
 
 <script>
+import ProductSearch from "../components/ProductSearch.vue";
+import ProductList from "../components/ProductList.vue";
 export default {
   name: "Home",
+  components: {
+    ProductSearch,
+    ProductList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
+>
