d22a8378aa3a20dcb48b8c95c294d8652d4cc818
marlenesedano <marlenesedanodasilva@gmail.com>
2022-01-25 20:35:24 -0400

    feat: add form with vuex

--- src/components/CreateLogin.vue
+++ src/components/CreateLogin.vue
@@ -1,11 +1,48 @@
 <template>
-  <div></div>
+  <section>
+    <h1>Crie a sua conta</h1>
+    <transition mode="out-in">
+      <button v-if="!create" class="btn create" @click="create = true">
+        Criar conta
+      </button>
+      <UserForm v-else>
+        <button class="btn btn-form">Criar Usuário</button>
+      </UserForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UserForm from "../components/UserForm.vue";
+
 export default {
   name: "CreateLogin",
+  components: {
+    UserForm,
+  },
+  data() {
+    return {
+      create: false,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 15px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form>
+    <label for="name">Nome</label>
+    <input type="text" name="name" id="name" v-model="name" />
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" v-model="email" />
+    <label for="password">Senha</label>
+    <input type="password" name="password" id="password" v-model="password" />
+    <label for="cep">Cep</label>
+    <input type="text" name="cep" id="cep" v-model="cep" />
+    <label for="number">Número</label>
+    <input type="number" name="number" id="number" v-model="number" />
+    <label for="street">Rua</label>
+    <input type="text" name="street" id="street" v-model="street" />
+    <label for="district">Bairro</label>
+    <input type="text" name="district" id="district" v-model="district" />
+    <label for="city">City</label>
+    <input type="text" name="city" id="city" v-model="city" />
+    <label for="state">Estado</label>
+    <input type="text" name="state" id="state" v-model="state" />
+    <div class="button">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+import { mapFields } from "../helpers";
+export default {
+  computed: {
+    ...mapFields({
+      fields: [
+        "name",
+        "email",
+        "password",
+        "cep",
+        "number",
+        "street",
+        "district",
+        "city",
+        "state",
+      ],
+      base: "user",
+      mutation: "UPDATE_USER",
+    }),
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  align-items: center;
+}
+
+.button {
+  grid-column: 2;
+  margin-top: 10px;
+}
+</style>
